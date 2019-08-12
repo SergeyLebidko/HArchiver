0fef06f40085b2fddfe679dc4d154981b9ad95be
SergeyLebidko <it.sergeyler@mail.ru>
2019-08-12 15:40:10 +0300

    Созданы диалоги выбора файлов

--- src/main/java/harchiver/GUI.java
+++ src/main/java/harchiver/GUI.java
@@ -1,9 +1,11 @@
 package harchiver;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class GUI {
 
@@ -13,10 +15,40 @@ public class GUI {
     private JFrame frm;
     private JTextArea outputArea;
 
+    private JFileChooser packFileChooser;
+    private JFileChooser unpackFileChooser;
     private JButton toArchiveBtn;
     private JButton fromArchiveBtn;
 
+    private class ArchiveFileFilter extends FileFilter {
+
+        @Override
+        public boolean accept(File f) {
+            String extension = getFileExtension(f);
+            if (extension == null) return true;
+            return extension.equals("lsa");
+        }
+
+        @Override
+        public String getDescription() {
+            return "Архивы lsa";
+        }
+
+        //Метод возвращает расширение файла file. Если передана ссылка на каталог - возвращает null
+        private String getFileExtension(File file) {
+            if (file.isDirectory()) return null;
+            String nameFile = file.getName();
+            int dotPos = nameFile.lastIndexOf(".");
+            if ((dotPos == (-1)) | (dotPos == 0) | (dotPos == (nameFile.length() - 1))) return "";
+            return (nameFile.substring(dotPos + 1)).toLowerCase();
+        }
+
+    }
+
     public GUI() {
+        UIManager.put("FileChooser.cancelButtonText", "Отмена");
+        UIManager.put("FileChooser.directoryOpenButtonText", "Открыть");
+
         frm = new JFrame("HArchiver");
         frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frm.setSize(w, h);
@@ -44,6 +76,13 @@ public class GUI {
         outputArea.setEditable(false);
         contentPane.add(new JScrollPane(outputArea), BorderLayout.CENTER);
 
+        packFileChooser = new JFileChooser();
+        packFileChooser.setDialogTitle("Выберите файл");
+
+        unpackFileChooser = new JFileChooser();
+        unpackFileChooser.setDialogTitle("Выберите файл");
+        unpackFileChooser.setFileFilter(new ArchiveFileFilter());
+
         frm.setContentPane(contentPane);
         frm.setVisible(true);
     }
@@ -51,14 +90,20 @@ public class GUI {
     private ActionListener pack = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            print("Запаковать файл...");
+            int result = packFileChooser.showDialog(frm, "Выбрать");
+            if (result != JFileChooser.APPROVE_OPTION) return;
+
+            File selectedFile = packFileChooser.getSelectedFile();
         }
     };
 
     private ActionListener unpack = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            print("Распаковать файл...");
+            int result = unpackFileChooser.showDialog(frm, "Выбрать");
+            if (result != JFileChooser.APPROVE_OPTION) return;
+
+            File selectedFile = packFileChooser.getSelectedFile();
         }
     };
 
